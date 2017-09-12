6fd0e871477ea7a0891d2c33440a298417b5dae7
Greta Moseley <greta.moseley@gmail.com>
2017-09-12 12:12:47 -0500

    "modify database file based on discussion with product owners to reflect new join table for employees-computers connection and to delete unnecessary table columns"

--- db/build-db.js
+++ db/build-db.js
@@ -2,19 +2,18 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('bangazon.sqlite');
 
 db.serialize(function(){
-
-    //drop existing tables to get up-to-date info  -- not sure of order to put in
-    db.run(`DROP TABLE IF EXISTS departments`);
-    db.run(`DROP TABLE IF EXISTS productTypes`);
-    db.run(`DROP TABLE IF EXISTS productOrders`);
-    db.run(`DROP TABLE IF EXISTS products`);
-    db.run(`DROP TABLE IF EXISTS orders`);
-    db.run(`DROP TABLE IF EXISTS paymentOptions`);
-    db.run(`DROP TABLE IF EXISTS employees`);
-    db.run(`DROP TABLE IF EXISTS employeeTraining`);
-    db.run(`DROP TABLE IF EXISTS training`);
-    db.run(`DROP TABLE IF EXISTS computers`);
     db.run(`DROP TABLE IF EXISTS users`);
+    db.run(`DROP TABLE IF EXISTS computers`);
+    db.run(`DROP TABLE IF EXISTS training`);
+    db.run(`DROP TABLE IF EXISTS employeeTraining`);
+    db.run(`DROP TABLE IF EXISTS employees`);
+    db.run(`DROP TABLE IF EXISTS paymentOptions`);
+    db.run(`DROP TABLE IF EXISTS orders`);
+    db.run(`DROP TABLE IF EXISTS products`);
+    db.run(`DROP TABLE IF EXISTS productOrders`);
+    db.run(`DROP TABLE IF EXISTS productTypes`);
+    db.run(`DROP TABLE IF EXISTS departments`);
+    db.run(`DROP TABLE IF EXISTS computerEmployees `);
 
     db.run(`CREATE TABLE IF NOT EXISTS users(
         user_id INTEGER PRIMARY KEY NOT NULL,
@@ -26,39 +25,34 @@ db.serialize(function(){
         city TEXT NOT NULL,
         state TEXT NOT NULL,
         postal_code INTEGER NOT NULL,
-        phoneNumber TEXT NOT NULL
-
+        phone TEXT NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS computers(
-    computerId INTEGER PRIMARY KEY,
-    datePurchased TEXT,
-    dateDecom TEXT,
-    employee_id INTEGER
+        computer_id INTEGER PRIMARY KEY NOT NULL,
+        purchase_date TEXT NOT NULL,
+        decomission_date TEXT NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS training(
-        trainProgId INTEGER PRIMARY KEY,
-        trainProgName TEXT,
-        trainProgStart TEXT,
-        trainProgEnd TEXT,
-        maxAttendees INTEGER,
-        employeesAttending INTEGER,
-        employeesCompleted INTEGER
+        program_id INTEGER PRIMARY KEY NOT NULL,
+        program_name TEXT NOT NULL,
+        start_date TEXT NOT NULL,
+        end_date TEXT NOT NULL,
+        max_attendees INTEGER NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS employeeTraining(
-        empTrainId INTEGER PRIMARY KEY,
-        employee_id INTEGER,
-        trainProgId INTEGER,
-        complete INTEGER
+        empTrain_id INTEGER PRIMARY KEY NOT NULL,
+        employee_id INTEGER NOT NULL,
+        training_program_Id INTEGER NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS employees(
-        employeeId INTEGER PRIMARY KEY,
-        department_id INTEGER,
-        employeeName TEXT,
-        empSupervisor INTEGER
+        employee_id INTEGER PRIMARY KEY NOT NULL,
+        department INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        hire_date TEXT NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS paymentOptions(
@@ -81,12 +75,12 @@ db.serialize(function(){
         seller_id INTEGER NOT NULL,
         product_name TEXT NOT NULL,
         description TEXT NOT NULL,
+        quantity_avail INTEGER NOT NULL,
         price REAL NOT NULL
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS productOrders(
         order_id INTEGER NOT NULL,
-        quantity INTEGER NOT NULL,
         product_id INTEGER NOT NULL,
         line_item_id INTEGER PRIMARY KEY NOT NULL
     )`);
@@ -97,9 +91,16 @@ db.serialize(function(){
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS departments(
-        departmentId INTEGER PRIMARY KEY,
-        supervisor INTEGER,
-        departmentName TEXT,
-        budget REAL
+        department_id INTEGER PRIMARY KEY NOT NULL,
+        supervisor_id INTEGER NOT NULL,
+        dept_name TEXT NOT NULL,
+        budget REAL NOT NULL
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS computerEmployees(
+        computer_user_id INTEGER PRIMARY KEY,
+        employee_id INTEGER NOT NULL,
+        issued_date TEXT NOT NULL,
+        returned_date TEXT NOT NULL
     )`);
 });
