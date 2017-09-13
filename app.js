8f6fba85df9774162867e9202ad99b76b3bc0aac
Greta Moseley <greta.moseley@gmail.com>
2017-09-13 15:00:55 -0500

    "add route for user get through app, index files and user.js in model folder, userCtrl in controller folder,  index.js in route folder, also change package json to use npm start command"

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+let express = require('express');
+let app = express();
+require('dotenv').config();
+let bodyParser = require('body-parser');
+let routes = require('./routes/');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next)=>{
+    res.end({
+        usage:"/bangazonAPI/v1/"
+    })
+})
+app.use(`/bangazonAPI/v1/`, routes); //require in routes so it will look in index.js for the next step
+
+
+//other middleware?
+
+app.use((req, res, next)=>{
+    let error = new Error('sorry, not found.');
+    error.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next)=>{
+    res.status(err.status||500);
+    res.json({
+	message:"A problem occurred.",
+	err:err
+    })
+});
+
+app.listen(process.env.port || 8080, ()=>{
+    console.log("Listening on port specified.");
+});
