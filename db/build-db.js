a8b5cd53aa399b545ff14e030ad0080518ca6d6f
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-09-13 09:52:11 -0500

    fixed typo

--- db/build-db.js
+++ db/build-db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(__dirname'/bangazon.sqlite');
+const db = new sqlite3.Database(__dirname+'/bangazon.sqlite');
 
 const { generateProdTypes, generateProducts } = require('./products-db.js');
 const { generateUsers } = require('./users-db');
