c9c9e78f2ee6df677bbb42eb23eff90c721a1ed7
An-AngryBear <monahajt@gmail.com>
2017-09-15 10:21:23 -0500

    added functionality for active query, checks for a query string of active=boolean, if no query will cascade to the next route

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,22 +1,23 @@
  'use strict'
-const{getUsers, getOneUser, postUserObj, putUserObj}= require('../models/User'); //and whatever other methods exported
+const{ getUsers, getOneUser, postUserObj, putUserObj, getUsersNoOrders } = require('../models/User'); //and whatever other methods exported
 
-module.exports.getAll=(req, res, next)=>{
+module.exports.getAll = (req, res, next) => {
+    console.log(req.query);
     getUsers()//from models folder
-    .then((users)=>{
+    .then( (users)=>{
         res.status(200).json(users);
     })
-    .catch((err)=>{
+    .catch( (err)=>{
         next(err);
     })
 };
 
-module.exports.getOneUserById =(req, res, next)=>{
+module.exports.getOneUserById = (req, res, next) => {
     getOneUser(req.params.id)//method from User.js
-    .then((user)=>{
+    .then( (user) => {
         res.status(200).json(user);
     })
-    .catch((err)=>{
+    .catch( (err) => {
         next(err);
     })
 }
@@ -24,10 +25,10 @@ module.exports.getOneUserById =(req, res, next)=>{
 //MAKE SURE to set POSTMAN body to JSON not text.
 module.exports.postUser = (req, res, next) => {
     postUserObj(req.body)
-    .then((data) => {
+    .then( (data) => {
         res.status(200).end("Posted.");
     })
-    .catch((err)=>{
+    .catch( (err)=>{
         next(err);
     })
 }
@@ -35,11 +36,31 @@ module.exports.postUser = (req, res, next) => {
 
 module.exports.putUser = (req, res, next) => {
     putUserObj(req.params.id, req.body)
-    .then((data) => {
+    .then( (data) => {
         res.status(200).end("Updated.");
     })
-    .catch((err)=>{
+    .catch( (err)=>{
         next(err);
     })
 }
 
+module.exports.getUsersWithNoOrders = (req, res, next) => {
+    let queryBoolean = checkForQuery(req.query) ? true : false;
+    if(queryBoolean && req.query.hasOwnProperty('active')) {
+        console.log("yay");
+        getUsersNoOrders(req.query)
+        .then( (users) => {
+            res.status(200).json(users);
+        })
+        .catch( (err)=> {
+            next(err);
+        })
+    } else {
+        next();
+    }
+}
+
+let checkForQuery = (query) => {
+    let queryBoolean = Object.keys(query).length > 0 ? true : false;
+    return queryBoolean;
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const{Router}= require('express');
+const { Router } = require('express');
 const router = Router();
 
 //require in controller method calls
-const{getAll, getOneUserById, postUser, putUser}= require('../controllers/usersCtrl');//along with methods for post put etc...
+const { getAll, getOneUserById, postUser, putUser, getUsersWithNoOrders } = require('../controllers/usersCtrl');//along with methods for post put etc...
 
+router.get('/users', getUsersWithNoOrders);
 router.get('/users', getAll);//if this route, get all
 router.get('/users/:id', getOneUserById);//if this route, get one using id passed in url
 router.post('/users', postUser);
