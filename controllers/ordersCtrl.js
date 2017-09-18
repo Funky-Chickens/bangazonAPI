0228e0b5a52ae88c670a691ecf908c77b77766be
An-AngryBear <monahajt@gmail.com>
2017-09-18 10:28:58 -0500

    make order controller uniform

--- controllers/ordersCtrl.js
+++ controllers/ordersCtrl.js
@@ -1,45 +1,38 @@
  'use strict'
-const{getOrders, getOneOrder, postOrderObj, postProdOrderObj, deleteOneOrder, deleteOneProdOrder, putOrder}= require('../models/Order');
+const { getOrders, getOneOrder, postOrderObj, postProdOrderObj, deleteOneOrder, deleteOneProdOrder, putOrder, getUsersOrders } = require('../models/Order');
 
 module.exports.getAll=(req, res, next)=>{
     getOrders()//from models folder
-    .then((orders)=>{
+    .then( (orders) => {
         res.status(200).json(orders);
     })
-    .catch((err)=>{
-        next(err);
+    .catch( (err) => next(err));
     })
 };
 
 module.exports.getOneOrderById =(req, res, next)=>{
     getOneOrder(req.params.id)//method from User.js
-    .then((order)=>{
+    .then( (order) => {
         res.status(200).json(order);
     })
-    .catch((err)=>{
-        next(err);
-    })
+    .catch( (err) => next(err));
 }
 
 //MAKE SURE the dev sets POSTMAN to JSON not text.
 module.exports.postOrder = (req, res, next) => {
     postOrderObj(req.body)
-    .then((data) => {
+    .then( (data) => {
         res.status(200).end('order posted sucessfully');
     })
-    .catch((err)=>{
-        next(err);
-    })
+    .catch( (err) => next(err));
 }
 
 module.exports.putOrder = (req, res, next) => {
     putOrder(req.params.id, req.body)
-    .then((data) => {
+    .then( (data) => {
         res.status(200).end();
     })
-    .catch((err)=>{
-        next(err);
-    })
+    .catch( (err) => next(err));
 }
 
 module.exports.deleteOneOrder = ({params: {id}}, res, next) => {
@@ -48,16 +41,14 @@ module.exports.deleteOneOrder = ({params: {id}}, res, next) => {
         res.status(200).end();
     })
     .catch( (err) => next(err));
-};
+}
 
 module.exports.postProdOrder = (req, res, next) => {
     postProdOrderObj(req.body)
-    .then((data) => {
+    .then( (data) => {
         res.status(200).end('product order posted sucessfully');
     })
-    .catch((err)=>{
-        next(err);
-    })
+    .catch( (err) => next(err));
 }
 
 module.exports.deleteOneProdOrder = ({params: {id}}, res, next) => {
